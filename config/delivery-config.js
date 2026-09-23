0c21c5caf2a180c2cde2e5559e40d5109c4eaaa1
ladima75-source <ladima75@gmail.com>
2026-09-23 10:10:21 +0300

    Define BB610 order and free shipping rules

--- config/delivery-config.js
+++ config/delivery-config.js
@@ -1,4 +1,10 @@
 window.BB610_DELIVERY_CONFIG = Object.freeze({
+  orderRules: Object.freeze({
+    minOrderUah: 500,
+    freeShippingThresholdUah: 2500,
+    freeShippingMaxChargeableWeightKg: 10,
+    freeShippingProvider: 'nova_poshta'
+  }),
   providers: {
     pickup_dnipro: {label:'Самовивіз у Дніпрі',service:'pickup'},
     delivery_dnipro: {label:'Доставка по Дніпру',service:'courier'},
